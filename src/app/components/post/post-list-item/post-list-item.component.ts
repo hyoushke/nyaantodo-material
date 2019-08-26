cd2fe4088cd390adb01df60b180ab1c0a17cdc37
hyoushke <hyoushke@gmail.com>
2019-08-26 11:21:07 -0700

    fix @Input passing of components post-list and post-list-item

--- src/app/components/post/post-list-item/post-list-item.component.ts
+++ src/app/components/post/post-list-item/post-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-post-list-item',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list-item.component.scss']
 })
 export class PostListItemComponent implements OnInit {
+
+  @Input() postitem;
 
   constructor() { }
 
